4bec15eda769d7b7cc8d85bb03e4f28524345097
Flash31_ <arterentev128@gmail.com>
2024-01-21 00:17:08 +0300

    Add project files.

---
+++ TelegramBot/GPTBot.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+class GPTBot
+{
+    public static async Task<string> SendCompletionRequest(string question)
+    {
+        var prompt = new
+        {
+            modelUri = "gpt://b1gt4nscm4mtcv853jvr/yandexgpt-lite",
+            completionOptions = new
+            {
+                stream = false,
+                temperature = 0.6,
+                maxTokens = "2000"
+            },
+            messages = new[]
+            {
+                new { role = "user", text = $"{question}" },
+            }
+        };
+
+        var url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion";
+        var apiKey = "AQVNyn0pq-53tMTeBI7FQk1clrHQmz7e6nhBacY_";
+
+        using(var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Api-Key", apiKey);
+
+            var content = new StringContent(JsonConvert.SerializeObject(prompt), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, content);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var resultJson = await response.Content.ReadAsStringAsync();
+                var resultDocument = JsonDocument.Parse(resultJson);
+                var textProperty = resultDocument.RootElement.GetProperty("result")
+                    .GetProperty("alternatives")[0]
+                    .GetProperty("message")
+                    .GetProperty("text");
+
+                return textProperty.GetString();
+            }
+            else
+            {
+                return $"Error: {response.StatusCode}";
+            }
+        }
+    }
+}
+
+class Result
+{
+    [JsonPropertyName("alternatives")]
+    public Alternatives Alternatives { get; set; } = new();
+
+    [JsonPropertyName("usage")]
+    public Usage Usage { get; set; } = new();
+
+}
+
+class Alternatives
+{
+    [JsonPropertyName("message")]
+    public Message Message { get; set; } = new();
+
+    [JsonPropertyName("status")]
+    public string Status { get; set; } = "";
+}
+
+class Usage
+{
+    [JsonPropertyName("inputTextTokens")]
+    public string InputTextTokens { get; set; } = "";
+
+    [JsonPropertyName("completionTokens")]
+    public string CompletionTokens { get; set; } = "";
+
+    [JsonPropertyName("totalTokens")]
+    public string TotalTokens { get; set; } = "";
+}
+
+class Message
+{
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+}
+
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new TelegramBotClient("6791814675:AAFdzNpWAJFB7EXxh0gVmmrp_89TPn3EHKQ");
+        client.StartReceiving(Update, Error);
+        Console.ReadLine();
+    }
+
+    private async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+    {
+        string answer = "Error";
+        var message = update.Message;
+        
+        if(message?.Text != null)
+        {
+            await Console.Out.WriteLineAsync($"{message.Chat.FirstName}   |   {message.Text}");
+
+            answer = await GPTBot.SendCompletionRequest(message.Text);
+
+            await botClient.SendTextMessageAsync(message.Chat.Id, answer);
+            await Console.Out.WriteLineAsync($"bot: {answer}");
+
+            //if(message.Text == "Здарова" || message.Text == "здарова")
+            //{
+            //    answer = "Корова";
+            //    await botClient.SendTextMessageAsync(message.Chat.Id, answer);
+            //    await Console.Out.WriteLineAsync($"bot: {answer}");
+            //    return;
+            //}
+
+            //if(message.Text == "Изи" || message.Text == "изи")
+            //{
+            //    answer = "сосм";
+            //    await botClient.SendTextMessageAsync(message.Chat.Id, answer);
+            //    await Console.Out.WriteLineAsync($"bot: {answer}");
+            //    return;
+            //}
+
+            //if(message.Text == "Корова" || message.Text == "корова")
+            //{
+            //    answer = "Здорова";
+            //    await botClient.SendTextMessageAsync(message.Chat.Id, answer);
+            //    await Console.Out.WriteLineAsync($"bot: {answer}");
+            //    return;
+            //}
+            //if((message.Text == "Привет" || message.Text == "привет"))
+            //{
+            //    answer = "изи";
+            //    await botClient.SendTextMessageAsync(message.Chat.Id, answer);
+            //    await Console.Out.WriteLineAsync($"bot: {answer}");
+            //    return;
+            //}
+
+
+        }
+    }
+
+
+    private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+        Console.WriteLine($"Произошла ошибка: {exception.Message}");
+        return Task.CompletedTask;
+        //throw new NotImplementedException();
+    }
+
+}
